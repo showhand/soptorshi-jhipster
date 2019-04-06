4681375be31ab27050801bbc7cc73914bea17dac
monjurmorshed793@gmail.com <monjurmorshed793@gmail.com>
2019-04-06 07:22:55 +0600

    employee-management-module added.

---
+++ src/main/webapp/app/employee-management/employee-management.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeManagementRoutingModule } from './employee-management-routing.module';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule, EmployeeManagementRoutingModule]
+})
+export class EmployeeManagementModule {}
