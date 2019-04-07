d88c4f16962ba875b877ca2f42ba824badc2433e
munna <monjurmorshed793@gmail.com>
2019-04-07 19:11:42 +0600

    Selected tab related issue fixed.

--- src/main/webapp/app/entities/employee/employee-management.component.ts
+++ src/main/webapp/app/entities/employee/employee-management.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ViewChild } from '@angular/core';
 import { JhiAlertService } from 'ng-jhipster';
 import { ActivatedRoute } from '@angular/router';
 import { IEmployee } from 'app/shared/model/employee.model';
 import { NgbTab, NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 import { EmployeeService } from 'app/entities/employee/employee.service';
+import { Table } from 'primeng/table';
 
 @Component({
     selector: 'jhi-employee-management',
     templateUrl: './employee-management.component.html',
     styles: []
 })
-export class EmployeeManagementComponent implements OnInit {
+export class EmployeeManagementComponent implements OnInit, AfterContentInit {
     employee: IEmployee;
+    activeTabId: string;
     @ViewChild('#tabSet') tabSet: NgbTabset;
     constructor(
         protected jhiAlertService: JhiAlertService,
@@ -23,10 +25,13 @@ export class EmployeeManagementComponent implements OnInit {
         this.activatedRoute.data.subscribe(({ employee }) => {
             this.employee = employee;
         });
+        this.activeTabId = this.employeeService.selectedTabId;
     }
 
-    tabSelected($event: Event) {
-        this.employeeService.selectedTabId = $event.type;
+    ngAfterContentInit(): void {}
+
+    tabSelected($event: any) {
+        this.employeeService.selectedTabId = $event.nextId;
     }
 
     previousState() {
