f6647f845d0d317e16f8f1e42e7606e7ebcdb038
monju <monjurmorshed793@gmail.com>
2019-05-26 20:25:33 +0600

    routine tracing disabled.

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
                 },
                 ...LAYOUT_ROUTES
             ],
-            { useHash: true, enableTracing: DEBUG_INFO_ENABLED, scrollPositionRestoration: 'enabled' }
+            { useHash: true, enableTracing: false, scrollPositionRestoration: 'enabled' }
         )
     ],
     exports: [RouterModule]
