3944326da785cc9c8a8bd2e7a354b03a80798f30
monju <monjurmorshed793@gmail.com>
2019-09-03 21:47:34 +0600

    entity module refactored.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -481,10 +481,6 @@ import { requisitionDetailsExtendedPopupRoute, requisitionDetailsExtendedRoute }
             {
                 path: 'stock-out-item',
                 loadChildren: './stock-out-item/stock-out-item.module#SoptorshiStockOutItemModule'
-            },
-            {
-                path: 'quotation-details',
-                loadChildren: './quotation-details/quotation-details.module#SoptorshiQuotationDetailsModule'
             }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
