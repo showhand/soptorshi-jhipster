6183d358960d8dd475ed4eec1a18a9e96aaed132
kawsurilu <kawsurilu@yahoo.com>
2019-12-23 11:00:12 +0600

    modified view for stock in process

--- src/main/webapp/app/entities/stock-in-process-extended/stock-in-process-extended.component.ts
+++ src/main/webapp/app/entities/stock-in-process-extended/stock-in-process-extended.component.ts
@@ -5,12 +5,18 @@ import { JhiAlertService, JhiEventManager, JhiParseLinks } from 'ng-jhipster';
 import { AccountService } from 'app/core';
 import { StockInProcessExtendedService } from './stock-in-process-extended.service';
 import { StockInProcessComponent } from 'app/entities/stock-in-process';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { IStockInProcess } from 'app/shared/model/stock-in-process.model';
 
 @Component({
     selector: 'jhi-stock-in-process-extended',
     templateUrl: './stock-in-process-extended.component.html'
 })
 export class StockInProcessExtendedComponent extends StockInProcessComponent {
+    predicate: any;
+    reverse: any;
+    status: string;
+
     constructor(
         protected stockInProcessService: StockInProcessExtendedService,
         protected jhiAlertService: JhiAlertService,
@@ -20,5 +26,22 @@ export class StockInProcessExtendedComponent extends StockInProcessComponent {
         protected accountService: AccountService
     ) {
         super(stockInProcessService, jhiAlertService, eventManager, parseLinks, activatedRoute, accountService);
+        this.predicate = 'processStartedOn';
+        this.reverse = false;
+    }
+
+    protected hunt() {
+        this.stockInProcesses = [];
+        this.stockInProcessService
+            .query({
+                'status.equals': this.status,
+                page: this.page,
+                size: this.itemsPerPage,
+                sort: this.sort()
+            })
+            .subscribe(
+                (res: HttpResponse<IStockInProcess[]>) => this.paginateStockInProcesses(res.body, res.headers),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
     }
 }
