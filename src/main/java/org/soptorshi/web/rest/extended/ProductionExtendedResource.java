c94d4b79003e8a1f466d90d81aad19118a7c17c2
kawsurilu <kawsurilu@yahoo.com>
2020-02-17 20:08:31 +0600

    disable delete in production

--- src/main/java/org/soptorshi/web/rest/extended/ProductionExtendedResource.java
+++ src/main/java/org/soptorshi/web/rest/extended/ProductionExtendedResource.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.soptorshi.service.ProductionQueryService;
 import org.soptorshi.service.extended.ProductionExtendedService;
 import org.soptorshi.web.rest.ProductionResource;
+import org.soptorshi.web.rest.errors.BadRequestAlertException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,17 @@ public class ProductionExtendedResource extends ProductionResource {
 
     public ProductionExtendedResource(ProductionExtendedService productionService, ProductionQueryService productionQueryService) {
         super(productionService, productionQueryService);
+    }
+
+    /**
+     * DELETE  /productions/:id : delete the "id" production.
+     *
+     * @param id the id of the productionDTO to delete
+     * @return the ResponseEntity with status 200 (OK)
+     */
+    @DeleteMapping("/productions/{id}")
+    public ResponseEntity<Void> deleteProduction(@PathVariable Long id) {
+        log.debug("REST request to delete Production : {}", id);
+        throw new BadRequestAlertException("Delete operation is not allowed", ENTITY_NAME, "idnull");
     }
 }
