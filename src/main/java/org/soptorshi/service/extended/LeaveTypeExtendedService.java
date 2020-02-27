a468270370a52a87fac8441ccd95250a2b123874
kawsurilu <kawsurilu@yahoo.com>
2020-02-27 11:14:16 +0600

    fix build issue v2

--- src/main/java/org/soptorshi/service/extended/LeaveTypeExtendedService.java
+++ src/main/java/org/soptorshi/service/extended/LeaveTypeExtendedService.java
@@ -27,13 +27,10 @@ public class LeaveTypeExtendedService extends LeaveTypeService {
 
     private final LeaveTypeMapper leaveTypeMapper;
 
-    private final LeaveTypeSearchRepository leaveTypeSearchRepository;
-
     public LeaveTypeExtendedService(LeaveTypeRepository leaveTypeRepository, LeaveTypeMapper leaveTypeMapper, LeaveTypeSearchRepository leaveTypeSearchRepository) {
         super(leaveTypeRepository, leaveTypeMapper, leaveTypeSearchRepository);
         this.leaveTypeRepository = leaveTypeRepository;
         this.leaveTypeMapper = leaveTypeMapper;
-        this.leaveTypeSearchRepository = leaveTypeSearchRepository;
     }
 
     /**
@@ -58,7 +55,7 @@ public class LeaveTypeExtendedService extends LeaveTypeService {
         LeaveType leaveType = leaveTypeMapper.toEntity(leaveTypeDTO);
         leaveType = leaveTypeRepository.save(leaveType);
         LeaveTypeDTO result = leaveTypeMapper.toDto(leaveType);
-        leaveTypeSearchRepository.save(leaveType);
+        //leaveTypeSearchRepository.save(leaveType);
         return result;
     }
 }
