6eba8900f4d302abae47d4be47b38e1a933eb12b
kawsurilu <kawsurilu@yahoo.com>
2020-04-20 14:39:04 +0600

    add missing line

--- src/main/webapp/app/layouts/index.ts
+++ src/main/webapp/app/layouts/index.ts
@@ -9,3 +9,4 @@ export * from './navbar/navbar.route';
 export * from './profiles/page-ribbon.component';
 export * from './profiles/profile.service';
 export * from './profiles/profile-info.model';
+export * from './left-side-menu/left-side-menu.component';
