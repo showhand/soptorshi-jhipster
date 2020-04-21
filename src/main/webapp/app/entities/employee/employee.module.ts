842675fe5f4e9e9bc8cefd24c060d83070cbba36
monju <monjurmorshed793@gmail.com>
2020-04-22 00:38:47 +0600

    employee codes reverted to previous version.

--- src/main/webapp/app/entities/employee/employee.module.ts
+++ src/main/webapp/app/entities/employee/employee.module.ts
@@ -2,28 +2,35 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SoptorshiSharedModule } from 'app/shared';
-import {
-    EmployeeComponent,
-    EmployeeDetailComponent,
-    EmployeeUpdateComponent,
-    EmployeeDeletePopupComponent,
-    EmployeeDeleteDialogComponent,
-    employeeRoute,
-    employeePopupRoute
-} from './';
+import { EmployeeComponent, EmployeeDetailComponent, EmployeeUpdateComponent, employeeRoute, employeePopupRoute } from './';
+import { EmployeeManagementComponent } from 'app/entities/employee/employee-management.component';
 
 const ENTITY_STATES = [...employeeRoute, ...employeePopupRoute];
 
 @NgModule({
     imports: [SoptorshiSharedModule, RouterModule.forChild(ENTITY_STATES)],
-    declarations: [
+    /* declarations: [
         EmployeeComponent,
         EmployeeDetailComponent,
         EmployeeUpdateComponent,
         EmployeeDeleteDialogComponent,
-        EmployeeDeletePopupComponent
+        EmployeeDeletePopupComponent,
+        EmployeeManagementComponent
     ],
-    entryComponents: [EmployeeComponent, EmployeeUpdateComponent, EmployeeDeleteDialogComponent, EmployeeDeletePopupComponent],
+    entryComponents: [
+        EmployeeComponent,
+        EmployeeUpdateComponent,
+        EmployeeDeleteDialogComponent,
+        EmployeeDeletePopupComponent,
+        EmployeeManagementComponent
+    ],
+    exports: [
+        EmployeeComponent,
+        EmployeeUpdateComponent,
+        EmployeeDeleteDialogComponent,
+        EmployeeDeletePopupComponent,
+        EmployeeManagementComponent
+    ],*/
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SoptorshiEmployeeModule {}
